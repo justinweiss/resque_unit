29e30074d6f8491597d5bbec645d39b87936ef5f
Ryan Biesemeyer <ryan@yaauie.com>
2011-09-27 13:37:04 -0700

    add support for before_enqueue hooks

--- lib/resque_unit/resque.rb
+++ lib/resque_unit/resque.rb
@@ -132,6 +132,11 @@ module Resque
 
   def enqueue_unit(queue_name, hash)
     klass = constantize(hash["class"])
+    if @hooks_enabled
+      Plugin.before_enqueue_hooks(klass).each do |hook|
+        klass.send(hook, *hash["args"])
+      end
+    end
     queue(queue_name) << encode(hash)
     if @hooks_enabled
       Plugin.after_enqueue_hooks(klass).each do |hook|
